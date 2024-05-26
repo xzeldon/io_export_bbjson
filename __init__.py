f8b23e82ba489b67507035e1f772bb72ddf4df12
xzeldon <contact@zeldon.ru>
2024-05-26 14:27:13 +0300

    Blender 4.1
    - Blender 4.1 support
    - Bump version
    - Not merge obj.name and obj.data.name
    - Change limb names for automatic setup (arm_left instead left_arm, leg_right instead right_leg etc.)
    - Remove sleeping and flying poses for lack of use for my case

--- __init__.py
+++ __init__.py
@@ -1,49 +1,78 @@
 bl_info = {
     "name": "Blockbuster JSON export",
-    "author": "McHorse",
-    "version": (0, 1, 2),
-    "blender": (2, 77, 0),
+    "author": "McHorse, xzeldon",
+    "version": (0, 1, 3),
+    "blender": (2, 80, 0),
     "location": "File > Export",
     "description": "Exports JSON model out of Blender document for Blockbuster mod. Its main task is to create limbs and poses configuration to avoid doing it by hand.",
     "warning": "",
-    "category": "Export"
+    "category": "Export",
 }
 
 import bpy
 
-from bpy.props import (BoolProperty, StringProperty)
-from bpy_extras.io_utils import (ExportHelper, path_reference_mode)
+from bpy.props import BoolProperty, StringProperty
+from bpy_extras.io_utils import ExportHelper, path_reference_mode
+
 
 # Export panel
 class ExportOBJ(bpy.types.Operator, ExportHelper):
     # Panel's information
     bl_idname = "export_scene.bbjson"
-    bl_label = 'Export Blockbuster JSON'
-    bl_options = {'PRESET'}
+    bl_label = "Export Blockbuster JSON"
+    bl_options = {"PRESET"}
 
     # Panel's properties
     filename_ext = ".json"
-    filter_glob = StringProperty(default="*.json", options={'HIDDEN'})
-    use_selection = BoolProperty(name="Selection Only", description="Export selected objects only", default=False)
-    provides_mtl = BoolProperty(name="Provides Materials", description="Mark 'provides_mtl' as True", default=False)
+    filter_glob: StringProperty(default="*.json", options={"HIDDEN"})
+    use_selection: BoolProperty(
+        name="Selection Only", description="Export selected objects only", default=False
+    )
+    provides_mtl: BoolProperty(
+        name="Provides Materials",
+        description="Mark 'provides_mtl' as True",
+        default=False,
+    )
     path_mode = path_reference_mode
     check_extension = True
 
     def execute(self, context):
         from . import export
-        
-        keywords = self.as_keywords(ignore=("axis_forward", "axis_up", "check_existing", "filter_glob", "path_mode"))
-        
+
+        keywords = self.as_keywords(
+            ignore=(
+                "axis_forward",
+                "axis_up",
+                "check_existing",
+                "filter_glob",
+                "path_mode",
+            )
+        )
+
         return export.save(context, **keywords)
+
 
 # Register and stuff
 def menu_func_export(self, context):
     self.layout.operator(ExportOBJ.bl_idname, text="Blockbuster JSON (.json)")
 
+
+classes = (ExportOBJ,)
+
+
 def register():
-    bpy.utils.register_module(__name__)
-    bpy.types.INFO_MT_file_export.append(menu_func_export)
+    from bpy.utils import register_class
+
+    for cls in classes:
+        register_class(cls)
+
+    bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
+
 
 def unregister():
-    bpy.utils.unregister_module(__name__)
-    bpy.types.INFO_MT_file_export.remove(menu_func_export)
+    from bpy.utils import unregister_class
+
+    for cls in reversed(classes):
+        unregister_class(cls)
+
+    bpy.types.TOPBAR_MT_file_export.remove(menu_func_export)
--- export.py
+++ export.py
@@ -4,15 +4,21 @@ import ntpath
 
 import bpy
 
-from progress_report import ProgressReport, ProgressReportSubstep
+from bpy_extras.wm_utils.progress_report import (
+    ProgressReport,
+    ProgressReportSubstep,
+)
+
 
 def path_leaf(path):
     head, tail = ntpath.split(path)
     return tail or ntpath.basename(head)
 
+
 # Remove spaces from given string (so it would be spaceless)
 def name_compat(name):
-    return 'None' if name is None else name.replace(' ', '_')
+    return "None" if name is None else name.replace(" ", "_")
+
 
 def save(context, filepath, use_selection=True, provides_mtl=False):
     with ProgressReport(context.window_manager) as progress:
@@ -20,7 +26,7 @@ def save(context, filepath, use_selection=True, provides_mtl=False):
 
         # Exit edit mode before exporting, so current object states are exported properly.
         if bpy.ops.object.mode_set.poll():
-            bpy.ops.object.mode_set(mode='OBJECT')
+            bpy.ops.object.mode_set(mode="OBJECT")
 
         objects = context.selected_objects if use_selection else scene.objects
 
@@ -28,89 +34,80 @@ def save(context, filepath, use_selection=True, provides_mtl=False):
         write_file(context, filepath, objects, scene, provides_mtl, progress)
         progress.leave_substeps()
 
-    return {'FINISHED'}
+    return {"FINISHED"}
 
-def write_file(context, filepath, objects, scene, provides_mtl, progress=ProgressReport()):
+
+def write_file(
+    context, filepath, objects, scene, provides_mtl, progress=ProgressReport()
+):
     # bpy.ops.object.select_all(action='DESELECT')
 
-    with ProgressReportSubstep(progress, 2, "JSON export path: %r" % filepath, "JSON export finished") as subprogress1:
+    with ProgressReportSubstep(
+        progress, 2, "JSON export path: %r" % filepath, "JSON export finished"
+    ) as subprogress1:
         with open(filepath, "w", encoding="utf8", newline="\n") as f:
-            limbs = {
-                'anchor': {
-                    'opacity': 0
-                }
-            }
+            limbs = {"anchor": {"opacity": 0}}
             pose = {}
 
             for obj in objects:
                 if obj.type != "MESH":
                     continue
-                
-                name1 = obj.name
-                name2 = obj.data.name
 
-                if name1 == name2:
-                    name = name_compat(name1)
-                else:
-                    name = '%s_%s' % (name_compat(name1), name_compat(name2))
-                
+                name = name_compat(obj.name)
+                # name1 = obj.name
+                # name2 = obj.data.name
+
+                # if name1 == name2:
+                #     name = name_compat(name1)
+                # else:
+                #     name = "%s_%s" % (name_compat(name1), name_compat(name2))
+
                 cursor = obj.matrix_world.translation
                 x = cursor[0]
                 y = cursor[2]
                 z = cursor[1]
-                
-                limb = { 
-                    'origin': [x, y, -z],
-                    'smooth': True,
-                    'parent': 'anchor'
-                }
 
-                transform = {
-                    'translate': [x * 16, y * 16, z * 16]
-                }
+                limb = {"origin": [x, y, -z], "smooth": True, "parent": "anchor"}
+
+                transform = {"translate": [x * 16, y * 16, z * 16]}
 
                 # Some automatic setup of limbs based on name
-                if name1 == 'head':
-                    limb['looking'] = True
-                elif name1 == 'left_arm':
-                    limb['holding'] = 'left'
-                    limb['swinging'] = True
-                    limb['idle'] = True
-                    limb['invert'] = True
-                elif name1 == 'right_arm':
-                    limb['holding'] = 'right'
-                    limb['swinging'] = True
-                    limb['idle'] = True
-                    limb['swiping'] = True
-                elif name1 == 'right_leg':
-                    limb['swinging'] = True
-                    limb['invert'] = True
-                elif name1 == 'left_leg':
-                    limb['swinging'] = True
-                
+                if name == "head":
+                    limb["looking"] = True
+                elif name == "arm_left":
+                    limb["holding"] = "left"
+                    limb["swinging"] = True
+                    limb["idle"] = True
+                    limb["invert"] = True
+                elif name == "arm_right":
+                    limb["holding"] = "right"
+                    limb["swinging"] = True
+                    limb["idle"] = True
+                    limb["swiping"] = True
+                elif name == "leg_right":
+                    limb["swinging"] = True
+                    limb["invert"] = True
+                elif name == "leg_left":
+                    limb["swinging"] = True
+
                 limbs[name] = limb
                 pose[name] = transform
-            
+
             # Write JSON to the file
-            pose = {
-                'size': [0.6, 1.8, 0.6],
-                'limbs': pose
-            }
-            
+            pose = {"size": [0.6, 1.8, 0.6], "limbs": pose}
+
             data = {
-                'scheme': "1.3",
-                'providesObj': True,
-                'providesMtl': provides_mtl,
-                'name': path_leaf(filepath),
-                'limbs': limbs,
-                'poses': {
-                    'standing': pose,
-                    'sneaking': pose,
-                    'sleeping': pose,
-                    'flying': pose
-                }
+                "scheme": "1.3",
+                "providesObj": True,
+                "providesMtl": provides_mtl,
+                "name": path_leaf(filepath),
+                "limbs": limbs,
+                "poses": {
+                    "standing": pose,
+                    "sneaking": pose,
+                },
             }
-            
+
             f.write(json.dumps(data, indent=4))
 
         subprogress1.step("Finished exporting JSON")
